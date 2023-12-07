bb449cf410cd385ae8b12d17688a516c1483c338
Tallaks <tallaksmain@gmail.com>
2023-12-07 18:57:39 +0400

    Parallel executing of services

--- WeatherPackage/Scripts/Source/WeatherProvider.cs
+++ WeatherPackage/Scripts/Source/WeatherProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -12,38 +13,43 @@ namespace WeatherSystem
 
     public void AddWeatherService(IWeatherService service)
     {
-      _services.TryAdd(service.GetType(), service);
+      if (_services.TryAdd(service.GetType(), service))
+        Debug.Log($"Added weather service {service.GetType().Name}");
+      else
+        Debug.LogWarning($"Weather service {service.GetType().Name} already added");
     }
 
     public async Task<Weather> GetWeather(double latitude, double longitude, float timeout,
       CancellationToken cancellationToken)
     {
       var weather = new Weather();
-      foreach (IWeatherService service in _services.Values)
-      {
-        WeatherInfo weatherInfo;
-        try
-        {
-          weatherInfo = await service.GetWeatherInfo(latitude, longitude, timeout, cancellationToken);
-        }
-        catch (Exception e)
-        {
-          switch (e)
-          {
-            case OperationCanceledException _:
-              Debug.LogWarning("Operation was cancelled");
-              throw;
-            default:
-              Debug.LogError(
-                $"Exception occurred while getting weather info from service {service.GetType().Name}\n{e.Message}");
-              continue;
-          }
-        }
+      IEnumerable<Task> tasks = _services.Values.Select(service =>
+        GetWeatherInfoFromService(service, latitude, longitude, timeout, cancellationToken, weather));
+      await Task.WhenAll(tasks);
+      return weather;
+    }
 
+    private static async Task GetWeatherInfoFromService(IWeatherService service, double latitude, double longitude,
+      float timeout, CancellationToken cancellationToken, Weather weather)
+    {
+      try
+      {
+        WeatherInfo weatherInfo = await service.GetWeatherInfo(latitude, longitude, timeout, cancellationToken);
         weather.AddWeatherInfo(service, weatherInfo);
       }
-
-      return weather;
+      catch (Exception e)
+      {
+        switch (e)
+        {
+          case OperationCanceledException _:
+            Debug.LogWarning("Operation was cancelled");
+            throw;
+          default:
+            Debug.LogError(
+              $"Exception occurred while getting weather info from service {service.GetType().Name}\n{e.Message}");
+            break;
+        }
+      }
     }
   }
 }
--- WeatherPackage/Scripts/Tests/WeatherApi.cs
+++ WeatherPackage/Scripts/Tests/WeatherApi.cs
@@ -53,21 +53,17 @@ namespace Weather.Tests
       var mockWeatherService2 = new MockWeatherDelayService(2);
 
       weatherProvider.AddWeatherService(mockWeatherService1);
+      LogAssert.Expect(LogType.Warning, "Weather service MockWeatherDelayService already added");
       weatherProvider.AddWeatherService(mockWeatherService2);
-      float startTime = Time.time;
 
       // Act
       Task<WeatherSystem.Weather> weatherTask = weatherProvider.GetWeather(10, 10, 0, default);
       yield return new WaitUntil(() => weatherTask.IsCompleted);
-      float finishTime = Time.time;
 
       WeatherSystem.Weather weather = weatherTask.Result;
       // Assert
       Assert.NotNull(weather);
       Assert.False(weather.IsEmpty);
-      Assert.Less(finishTime - startTime, 3f); // if it takes more than 3 seconds, then the second service was called
-      Assert.Greater(finishTime - startTime,
-        1f); // if it takes less than 1 second, then at least one service was called
       Assert.AreEqual(weather.GetWeatherInfoFrom<MockWeatherDelayService>().Latitude, 10);
       Assert.AreEqual(weather.GetWeatherInfoFrom<MockWeatherDelayService>().Longitude, 10);
     }
